b794c7d6c45a5eb159b0acdfcf6ba285d4156dd7
Ilya Lisovitskyi <Vissho@users.noreply.github.com>
2022-02-21 18:19:21 +0700

    Chess: level Hard done

--- chess.c
+++ chess.c
@@ -1,60 +1,88 @@
 #include <ctype.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define coordinates 4
+#define COORDINATES 4
+#define FIELD 9
 
-void createchess(char** chess)
+void create_chess(char** chess)
 {
     int i, j;
-    char x = 96, z = 56;
-    for (i = 0; i < 9; i++) {
-        for (j = 0; j < 9; j++) {
-            if (j == 0 && z != 48) {
+    char x = 97, z = 56;
+    const int column_for_numbers = 0, column_for_letters = 8;
+    const int coord_for_black = 0, coord_for_white = 7;
+    const int coord_for_left_rook = 1, coord_for_right_rook = 8;
+    const int coord_for_left_kNight = 2, coord_for_right_kNight = 7;
+    const int coord_for_left_bishop = 3, coord_for_right_bishop = 6;
+    const int coord_for_queen = 4, coord_for_king = 5;
+    const int coord_for_black_pawn = 1, coord_for_white_pawn = 6,
+              beginning_of_the_line = 0;
+    for (i = 0; i < FIELD; i++) {
+        for (j = 0; j < FIELD; j++) {
+            if (j == column_for_numbers) {
                 chess[i][j] = z;
                 z--;
-            } else if (i == 8) {
+            } else if (i == column_for_letters) {
                 chess[i][j] = x;
                 x++;
-            } else if (i == 1 && j > 0) {
+            } else if (i == coord_for_black_pawn && j > beginning_of_the_line) {
                 chess[i][j] = 'p';
-            } else if (i == 6 && j > 0) {
+            } else if (i == coord_for_white_pawn && j > beginning_of_the_line) {
                 chess[i][j] = 'P';
-            } else if (i == 0 && (j == 1 || j == 8)) {
+            } else if (
+                    i == coord_for_black
+                    && (j == coord_for_left_rook
+                        || j == coord_for_right_rook)) {
                 chess[i][j] = 'r';
-            } else if (i == 0 && (j == 2 || j == 7)) {
+            } else if (
+                    i == coord_for_black
+                    && (j == coord_for_left_kNight
+                        || j == coord_for_right_kNight)) {
                 chess[i][j] = 'n';
-            } else if (i == 0 && (j == 3 || j == 6)) {
+            } else if (
+                    i == coord_for_black
+                    && (j == coord_for_left_bishop
+                        || j == coord_for_right_bishop)) {
                 chess[i][j] = 'b';
-            } else if (i == 0 && j == 4) {
+            } else if (i == coord_for_black && j == coord_for_queen) {
                 chess[i][j] = 'q';
-            } else if (i == 0 && j == 5) {
+            } else if (i == coord_for_black && j == coord_for_king) {
                 chess[i][j] = 'k';
-            } else if (i == 7 && (j == 1 || j == 8)) {
+            } else if (
+                    i == coord_for_white
+                    && (j == coord_for_left_rook
+                        || j == coord_for_right_rook)) {
                 chess[i][j] = 'R';
-            } else if (i == 7 && (j == 2 || j == 7)) {
+            } else if (
+                    i == coord_for_white
+                    && (j == coord_for_left_kNight
+                        || j == coord_for_right_kNight)) {
                 chess[i][j] = 'N';
-            } else if (i == 7 && (j == 3 || j == 6)) {
+            } else if (
+                    i == coord_for_white
+                    && (j == coord_for_left_bishop
+                        || j == coord_for_right_bishop)) {
                 chess[i][j] = 'B';
-            } else if (i == 7 && j == 4) {
+            } else if (i == coord_for_white && j == coord_for_queen) {
                 chess[i][j] = 'Q';
-            } else if (i == 7 && j == 5) {
+            } else if (i == coord_for_white && j == coord_for_king) {
                 chess[i][j] = 'K';
             } else {
                 chess[i][j] = ' ';
             }
         }
     }
-    chess[8][0] = ' ';
+    chess[column_for_letters][column_for_numbers] = ' ';
 }
 
-int checkrule(char** chess, int* rule)
+int check_rule(char** chess, int* rule, char type)
 {
-    int flag = 0;
+    int flag = 0, i;
     char c = chess[rule[0]][rule[1]];
     // rule[1] and rule[3] - буквы, rule[0] and rule[2] - цифры
-    printf("%d %d %d %d\n", rule[1], rule[0], rule[3], rule[2]);
-    if (c == 'P' || c == 'p') {
+    // printf("%d %d %d %d\n", rule[1], rule[0], rule[3], rule[2]);
+    if ((c == 'P' || c == 'p') && type == '-') {
         if (rule[1] != rule[3]) {
             flag++;
         }
@@ -74,32 +102,203 @@ int checkrule(char** chess, int* rule)
                 flag++;
             }
         }
+    } else if ((c == 'P' || c == 'p') && type == 'x') {
+        if (c == 'P') {
+            if (rule[0] - rule[2] != 1
+                || (rule[1] - rule[3] != 1 && rule[3] - rule[1] != 1)) {
+                flag++;
+            }
+        }
+        if (c == 'p') {
+            if (rule[2] - rule[0] != 1
+                || (rule[1] - rule[3] != 1 && rule[3] - rule[1] != 1)) {
+                flag++;
+            }
+        }
     }
     if (c == 'R' || c == 'r') {
+        if (rule[0] != rule[2]) {
+            if (rule[0] > rule[2]) {
+                for (i = rule[2] + 1; i < rule[0]; i++) {
+                    if (chess[i][rule[1]] != ' ') {
+                        flag++;
+                        break;
+                    }
+                }
+            } else {
+                for (i = rule[0] + 1; i < rule[2]; i++) {
+                    if (chess[i][rule[1]] != ' ') {
+                        flag++;
+                        break;
+                    }
+                }
+            }
+        } else if (rule[1] != rule[3]) {
+            if (rule[1] > rule[3]) {
+                for (i = rule[3] + 1; i < rule[1]; i++) {
+                    if (chess[rule[0]][i] != ' ') {
+                        flag++;
+                        break;
+                    }
+                }
+            } else {
+                for (i = rule[1] + 1; i < rule[3]; i++) {
+                    if (chess[rule[0]][i] != ' ') {
+                        flag++;
+                        break;
+                    }
+                }
+            }
+        }
         if (rule[1] != rule[3] && rule[0] != rule[2]) {
             flag++;
         }
     }
     if (c == 'N' || c == 'n') {
-        if (rule[1] - rule[3] == 1 || rule[1] - rule[3] == -1) {
-            if (rule[0] - rule[2] != 2 && rule[0] - rule[2] != -2) {
-                flag++;
-            }
-        } else if (rule[0] - rule[2] == 1 || rule[0] - rule[2] == -1) {
-            if (rule[1] - rule[3] != 2 && rule[1] - rule[3] != -2) {
-                flag++;
-            }
+        int x, y;
+        x = abs(rule[1] - rule[3]);
+        y = abs(rule[0] - rule[2]);
+        printf("%d-%d\n", x, y);
+        if ((x != 2 || y != 1) && (x != 1 || y != 2)) {
+            flag++;
         }
     }
     if (c == 'B' || c == 'b') {
+        int x = rule[3] - rule[1], y = rule[2] - rule[0];
+        int j;
+        if (x > 0 && y > 0) {
+            j = rule[0] + 1;
+            for (i = rule[1] + 1; i < rule[3]; i++) {
+                if (chess[j][i] != ' ') {
+                    flag++;
+                    break;
+                }
+                j++;
+            }
+        } else if (x > 0 && y < 0) {
+            j = rule[0] - 1;
+            for (i = rule[1] + 1; i < rule[3]; i++) {
+                if (chess[j][i] != ' ') {
+                    flag++;
+                    break;
+                }
+                j--;
+            }
+        } else if (x < 0 && y < 0) {
+            j = rule[0] - 1;
+            for (i = rule[1] - 1; i > rule[3]; i--) {
+                if (chess[j][i] != ' ') {
+                    flag++;
+                    break;
+                }
+                j--;
+            }
+        } else if (x < 0 && y > 0) {
+            j = rule[0] + 1;
+            for (i = rule[1] - 1; i > rule[3]; i--) {
+                if (chess[j][i] != ' ') {
+                    flag++;
+                    break;
+                }
+                j++;
+            }
+        }
         if (rule[1] - rule[3] != rule[0] - rule[2]
             && rule[1] - rule[3] != rule[2] - rule[0]) {
             flag++;
         }
     }
-    if (c == 'k' || c == 'K') {
+    if ((c == 'k' || c == 'K') && type == '-') {
         if (rule[1] - rule[3] > 1 || rule[3] - rule[1] > 1
             || rule[0] - rule[2] > 1 || rule[2] - rule[0] > 1) {
+            flag++;
+        }
+    }
+    if (c == 'q' || c == 'Q') {
+        int x, y;
+        x = abs(rule[1] - rule[3]);
+        y = abs(rule[0] - rule[2]);
+        if ((x == 2 && y == 1) || (x == 1 && y == 2)) {
+            flag++;
+        }
+        x = rule[3] - rule[1];
+        y = rule[2] - rule[0];
+        int j, i;
+        if (x > 0 && y > 0) {
+            j = rule[0] + 1;
+            for (i = rule[1] + 1; i < rule[3]; i++) {
+                if (chess[j][i] != ' ') {
+                    flag++;
+                    break;
+                }
+                j++;
+            }
+        } else if (x > 0 && y < 0) {
+            j = rule[0] - 1;
+            for (i = rule[1] + 1; i < rule[3]; i++) {
+                if (chess[j][i] != ' ') {
+                    flag++;
+                    break;
+                }
+                j--;
+            }
+        } else if (x < 0 && y < 0) {
+            j = rule[0] - 1;
+            for (i = rule[1] - 1; i > rule[3]; i--) {
+                if (chess[j][i] != ' ') {
+                    flag++;
+                    break;
+                }
+                j--;
+            }
+        } else if (x < 0 && y > 0) {
+            j = rule[0] + 1;
+            for (i = rule[1] - 1; i > rule[3]; i--) {
+                if (chess[j][i] != ' ') {
+                    flag++;
+                    break;
+                }
+                j++;
+            }
+        }
+        if (rule[0] == rule[2] || rule[1] == rule[3]) {
+            if (rule[0] != rule[2]) {
+                if (rule[0] > rule[2]) {
+                    for (i = rule[2] + 1; i < rule[0]; i++) {
+                        if (chess[i][rule[1]] != ' ') {
+                            flag++;
+                            break;
+                        }
+                    }
+                } else {
+                    for (i = rule[0] + 1; i < rule[2]; i++) {
+                        if (chess[i][rule[1]] != ' ') {
+                            flag++;
+                            break;
+                        }
+                    }
+                }
+            } else if (rule[1] != rule[3]) {
+                if (rule[1] > rule[3]) {
+                    for (i = rule[3] + 1; i < rule[1]; i++) {
+                        if (chess[rule[0]][i] != ' ') {
+                            flag++;
+                            break;
+                        }
+                    }
+                } else {
+                    for (i = rule[1] + 1; i < rule[3]; i++) {
+                        if (chess[rule[0]][i] != ' ') {
+                            flag++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        x = abs(rule[1] - rule[3]);
+        y = abs(rule[0] - rule[2]);
+        if (x != 0 && y != 0 && x != y) {
             flag++;
         }
     }
@@ -107,7 +306,17 @@ int checkrule(char** chess, int* rule)
     return flag;
 }
 
-void stepchess(char** chess, char* step)
+void free_and_exit(char** chess)
+{
+    int i;
+    for (i = 0; i < FIELD; i++) {
+        free(chess[i]);
+    }
+    free(chess);
+    exit(-1);
+}
+
+void step_chess(char** chess, char* step, int flag)
 {
     long int x = strlen(step);
     int stepF1, stepF2, stepS1, stepS2;
@@ -149,92 +358,132 @@ void stepchess(char** chess, char* step)
 
     if (x < 5 || x > 10) {
         printf("Вы ввели некорректный ход!\n");
-        exit(-1);
+        free_and_exit(chess);
     } else if (
             stepF2 > 7 || stepF2 < 0 || stepS2 > 7 || stepS2 < 0 || stepF1 > 8
             || stepF1 < 1 || stepS1 > 8 || stepS1 < 1) {
         printf("Вы вышли за пределы поля!\n");
-        exit(-1);
+        free_and_exit(chess);
+        ;
     } else if (figure != toupper(chess[stepF2][stepF1]) && x == 6) {
         if (chess[stepF2][stepF1] == ' ') {
             printf("На этом месте нет фигуру!\n");
+            free_and_exit(chess);
         } else {
             printf("Вы берете не ту фигуру!\n");
+            free_and_exit(chess);
         }
-        exit(-1);
     } else if (chess[stepF2][stepF1] == ' ') {
         printf("На этом месте нет фигуру!\n");
-        exit(-1);
+        free_and_exit(chess);
+    } else if (stepF1 == stepS1 && stepF2 == stepS2) {
+        printf("Вы не можете ходить на месте!\n");
+        free_and_exit(chess);
     } else if (
             (type == '-' && chess[stepS2][stepS1] != ' ')
             || (type == 'x' && chess[stepS2][stepS1] == ' ')
             || (type != '-' && type != 'x')) {
         printf("Вы используете некорректный тип хода!\n");
-        exit(-1);
+        free_and_exit(chess);
     } else if (
             x >= 7
             && (end1 != '+' && end1 != '#'
                 && (end1 != 'e' || end2 != '.' || end3 != 'p'
                     || end4 != '.'))) {
         printf("Вы используете некорректный тип хода!!\n");
-        exit(-1);
+        free_and_exit(chess);
+    } else if (type == 'x') {
+        if (isupper(chess[stepF2][stepF1]) != 0) {
+            if (isupper(chess[stepS2][stepS1]) != 0) {
+                printf("Нельзя рубить одинаковые по цвету фигуры!\n");
+                free_and_exit(chess);
+            }
+        } else if (islower(chess[stepF2][stepF1]) != 0) {
+            if (islower(chess[stepS2][stepS1]) != 0) {
+                printf("Нельзя рубить одинаковые по цвету фигуры!\n");
+                free_and_exit(chess);
+            }
+        }
+    } else if (flag == 1 && islower(chess[stepF2][stepF1])) {
+        printf("Вы ходите не по очерёдности!\n");
+        free_and_exit(chess);
+    } else if (flag == -1 && isupper(chess[stepF2][stepF1])) {
+        printf("Вы ходите не по очерёдности!\n");
+        free_and_exit(chess);
     }
 
-    int RULE[coordinates] = {stepF2, stepF1, stepS2, stepS1};
-    if (checkrule(chess, RULE) != 0) {
+    int RULE[COORDINATES] = {stepF2, stepF1, stepS2, stepS1};
+    if (check_rule(chess, RULE, type) != 0) {
         printf("Вы неправильно походили фигурой!\n");
-        exit(-1);
+        free_and_exit(chess);
     }
 
     chess[stepS2][stepS1] = chess[stepF2][stepF1];
     chess[stepF2][stepF1] = ' ';
 }
 
-void printchess(char** chess)
+void print_chess(char** chess)
 {
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
+    for (int i = 0; i < FIELD; i++) {
+        for (int j = 0; j < FIELD; j++) {
             printf("%c ", chess[i][j]);
         }
         printf("\n");
     }
 }
 
-int checkking(char** chess)
+int check_king(char** chess, char* step, int flag)
 {
-    int i, flag = 0;
-    for (i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (chess[i][j] == 'k' || chess[i][j] == 'K') {
-                flag++;
-            }
-        }
+    long int x = strlen(step);
+    char end1;
+    if (x >= 7) {
+        end1 = step[6];
     }
-    return flag;
+
+    int flag1 = 0;
+
+    if (end1 == '+') {
+        if (flag == 1) {
+            printf("Шах королю белых!\n");
+        } else if (flag == -1) {
+            printf("Шах королю чёрных!\n");
+        }
+    } else if (end1 == '#') {
+        if (flag == 1) {
+            printf("Шах и мат королю белых!\n");
+        } else if (flag == -1) {
+            printf("Шах и мат королю чёрных!\n");
+        }
+        flag1++; 
+    }
+
+    return flag1;
 }
 
 int main()
 {
-    int i;
+    int i, flag = 1;
     char** chess;
     char step[30];
-    chess = (char**)malloc(9 * sizeof(char*));
-    for (i = 0; i < 9; i++) {
-        chess[i] = (char*)malloc(9 * sizeof(char));
+    chess = (char**)malloc(FIELD * sizeof(char*));
+    for (i = 0; i < FIELD; i++) {
+        chess[i] = (char*)malloc(FIELD * sizeof(char));
     }
 
-    createchess(chess);
-    printchess(chess);
+    create_chess(chess);
+    print_chess(chess);
 
-    while (checkking(chess) == 2) {
+    do {
         scanf("%s", step);
-        stepchess(chess, step);
-        printchess(chess);
-    }
+        step_chess(chess, step, flag);
+        print_chess(chess);
+        flag = flag * (-1);
+    } while (check_king(chess, step, flag) == 0);
 
-    for (i = 0; i < 9; i++) {
+    for (i = 0; i < FIELD; i++) {
         free(chess[i]);
     }
     free(chess);
+
     return 0;
 }
